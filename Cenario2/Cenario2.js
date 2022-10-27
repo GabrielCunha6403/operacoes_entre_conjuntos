eda1fb59d921b23e9b1fa27e43f6020ddf2ba993
JOAO GABRIEL C JATAI <951549924@adm.unifor.br@DTEC65046811.unifor.br>
2022-10-27 17:20:44 -0300

    tratamento de repetição

--- Cenario2/Cenario2.js
+++ Cenario2/Cenario2.js
@@ -4,42 +4,42 @@ var qtdTentativas = 6;
 var contador = 0;
 
 function shuffle(array) {
-    array.sort(() => Math.random() - 0.5);
-    return array;
+  array.sort(() => Math.random() - 0.5);
+  return array;
 }
 
 function equalsCheck(array1, array2) {
-    for (let i = 0; i < array1.length; i++) {
-        if(array1[i] != array2[i])
-            return false;
-    }
-    return true;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] != array2[i]) return false;
+  }
+  return true;
 }
 
 for (let i = 0; i < 60; i++) {
-    vetorNumeros[i] = i + 1;
+  vetorNumeros[i] = i + 1;
 }
 
 /*for (let i = 0; i < 6; i++) {
     sorteio[i] = parseInt(prompt('Digite o ' + (i + 1) + 'º número:'));
 }*/
 
-
 var inicio = 0;
 var final = inicio + 6;
 var tentativa = shuffle(vetorNumeros).slice(inicio, final);
 
-do {
+function callFunction() {
+  do {
     console.log(contador);
-    if(final < 60){
-        inicio += 6;
-        final += 6;
-        tentativa = vetorNumeros.slice(inicio, final);
-        contador++;
+    if (final <= 60) {
+      tentativa = vetorNumeros.slice(inicio, final);
+      inicio += 6;
+      final += 6;
+      contador++;
     } else {
-        inicio = 0;
-        final = 6;
-        tentativa = shuffle(vetorNumeros).slice(inicio, final);
-        contador++;
+      inicio = 0;
+      final = 6;
+      tentativa = shuffle(vetorNumeros).slice(inicio, final);
+      contador++;
     }
-} while (!equalsCheck(tentativa, sorteio))
+  } while (!equalsCheck(tentativa, sorteio));
+}
